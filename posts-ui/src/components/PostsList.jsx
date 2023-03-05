b6a754063cf4aa382436c5736b856b8d9a857d9b
Luis Dourado <lfaddourado@gmail.com>
2023-03-05 17:54:29 -0300

    feat: handle loading state

--- posts-ui/src/components/PostsList.jsx
+++ posts-ui/src/components/PostsList.jsx
@@ -7,12 +7,15 @@ import classes from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsLoading(true);
       const response = await fetch('http://localhost:8080/posts');
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsLoading(false);
     }
 
     fetchPosts();
@@ -36,17 +39,22 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isLoading && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isLoading && posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>Não existem recados no momento.</h2>
           <p>Comece a adicionar alguns!</p>
+        </div>
+      )}
+      {isLoading && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>Carregando...</h2>
         </div>
       )}
     </>
