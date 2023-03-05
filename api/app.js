b4faf6ef083d1e5c93f49752381f872e01ed50dc
Luis Dourado <lfaddourado@gmail.com>
2023-03-05 16:59:04 -0300

    feat: configure cors

---
+++ api/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { getStoredPosts, storePosts } = require('./data/posts');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
