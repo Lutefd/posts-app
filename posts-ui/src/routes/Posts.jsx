c1fc89aab108680f70662b4db67ef4115a7d410b
Luis Dourado <lfaddourado@gmail.com>
2023-03-06 14:37:07 -0300

    feat: remove loader and fetch through useEffect

--- posts-ui/src/routes/Posts.jsx
+++ posts-ui/src/routes/Posts.jsx
@@ -1,24 +1,42 @@
 import PostsList from '../components/PostsList';
-import { Outlet } from 'react-router-dom';
+import Pagination from '../components/Pagination';
+import { useState, useEffect } from 'react';
+import { Outlet, useLoaderData } from 'react-router-dom';
+import classes from './Posts.module.css';
+
 function Posts() {
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`http://localhost:8000/posts?page=${page}`);
+      const resData = await response.json();
+      if (resData.posts !== undefined) {
+        setPosts(resData.posts);
+        setTotalPages(resData.totalPages);
+        console.log(totalPages);
+      } else {
+        setPosts([]);
+      }
+    }
+
+    fetchData();
+  }, [page]);
   return (
     <>
       <Outlet />
-      <main>
-        <PostsList />
+      <main className={classes.container}>
+        <PostsList page={page} posts={posts} />
+        <Pagination
+          totalPages={totalPages}
+          currentPage={page}
+          onPageChange={setPage}
+        />
       </main>
     </>
   );
 }
 
 export default Posts;
-
-export async function loader() {
-  const response = await fetch(`http://localhost:8080/posts`);
-  const resData = await response.json();
-  if (resData.posts !== undefined) {
-    return resData.posts;
-  } else {
-    return [];
-  }
-}
